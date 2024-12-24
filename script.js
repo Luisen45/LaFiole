0765924c77be889313e73f431796d014986424e7
Luisen45 <enriquecastanon54@gmail.com>
2024-12-24 01:30:25 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Aquí puedes agregar la lógica JavaScript que necesites.
+// Por ejemplo, podrías mostrar un mensaje en consola
+// para verificar que el archivo JS se está cargando correctamente.
+
+console.log("¡script.js cargado exitosamente!");
